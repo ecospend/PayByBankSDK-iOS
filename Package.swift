1da6909711bd1bf679542cd0427c38aecc941be9
Berk Akkerman <berk.akkerman@ecospend.com>
2021-12-14 16:40:44 +0300

    Add path to Package.swift

--- Package.swift
+++ Package.swift
@@ -20,9 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PaylinkMobileSDK-iOS",
-            dependencies: []),
-        .testTarget(
-            name: "PaylinkMobileSDK-iOSTests",
-            dependencies: ["PaylinkMobileSDK-iOS"]),
+            dependencies: [],
+            path: "PaylinkMobileSDK-iOS/Sources")
     ]
 )
